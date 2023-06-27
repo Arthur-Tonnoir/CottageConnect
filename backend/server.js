72bf11b32f0931381797ce796bd443e59af29742
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-06-27 09:54:39 +0200

    feat: add routes of Avi and Categorie :sparkles:

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
 app.use('/users', userRoutes);
 
+const aviRoutes = require('./routes/aviRoutes');
+app.use('/avis', aviRoutes);
+
+const categorieRoutes = require('./routes/categorieRoutes');
+app.use('/categories', categorieRoutes);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
