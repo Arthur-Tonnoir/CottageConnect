773e19da7e8e7655f9e621cee9ce76251eaca5df
Simart Lucie <lucie.srt@outlook.com>
2023-07-10 12:16:56 +0200

    finalisation homepage

--- src/component/Presentation/Presentation.jsx
+++ src/component/Presentation/Presentation.jsx
@@ -6,8 +6,25 @@ function Presentation() {
         <div className="containerPresentation">
             <h3>Qui sommes nous</h3>
             <div className="containerPresent">
-                <div className="presentationText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eum pariatur nemo repellendus velit, libero voluptas harum, laudantium labore quidem facilis. Eius natus repudiandae odio laboriosam quibusdam repellat, ullam, eum libero a voluptas, dolore accusamus. Iusto est quaerat aut mollitia, suscipit esse placeat sed ratione, voluptatem voluptatibus expedita assumenda. Distinctio eligendi nulla a ipsum, quaerat atque, non nisi aut eum ea quia cum. Error, laboriosam iure vitae sit alias, labore, maxime eius similique minus doloremque quod nobis asperiores eos exercitationem ipsa ratione est excepturi iste aperiam omnis voluptatibus commodi modi velit ab! Commodi corrupti adipisci repudiandae provident odit consectetur neque.</div>
-                <div className="presentationImg"><img src="" alt="Image" /></div>
+                <div className="presentationText"><h3>Bienvenue chez Casa Mea</h3>
+
+Votre destination privilégiée pour la location de gîtes exceptionnels. 
+<br/>
+<br/>
+Chez Casa Mea, nous comprenons l'importance de se sentir chez soi, même loin de chez soi. 
+C'est pourquoi nous avons soigneusement sélectionné une collection de gîtes de charme, chacun avec son propre caractère et son ambiance chaleureuse.
+<br/><br/>
+Nous nous engageons à vous offrir un séjour exceptionnel en mettant l'accent sur la qualité, le confort et le service personnalisé.
+<br/><br/>
+Notre équipe dévouée est là pour vous guider tout au long du processus de réservation et répondre à toutes vos questions.
+<br/><br/>
+Que vous soyez à la recherche d'une escapade tranquille à la campagne, d'une aventure en plein air ou d'une expérience urbaine animée, Casa Mea est là pour vous accompagner dans la réalisation de vos rêves de voyage.
+<br/><br/>
+Réservez dès maintenant votre gîte de rêve et préparez vous à vivre des moments inoubliables.
+<br/>
+Bienvenue chez Casa Mea, où chaque gîte est une maison pour vous.
+</div>
+                <div className="presentationImg"><img className="qsn" src="./img/qsn.webp" alt="Image" /></div>
             </div>
         </div>
     )
